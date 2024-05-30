6a18c3e2e7681e2c288b6a018505c79d65471bbc
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-30 22:47:17 +0300

    redesign. Admin chat with authorization middleware. Socket.io admin panel.

--- client/script.js
+++ client/script.js
@@ -1,21 +1,19 @@
 import {io} from "socket.io-client";
 
-const joinRoomButton = document.getElementById('room-button');
-const messageInput = document.getElementById('message-input');
-const roomInput = document.getElementById('room-input');
-const form = document.getElementById('form');
-
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
-    displayMessage('Connected to server. You ID is ' + socket.id);
+    const idPlace = document.getElementById('myID');
+    idPlace.textContent = socket.id;
 })
 
 socket.on('receive-message', (message) => {
     displayMessage(message);
 })
 
-form.addEventListener('submit', (e) => {
+const sendButton = document.getElementById('send-button');
+const messageInput = document.getElementById('message-input');
+sendButton.addEventListener('click', (e) => {
     e.preventDefault();
     const message = messageInput.value;
     const room = roomInput.value;
@@ -27,10 +25,12 @@ form.addEventListener('submit', (e) => {
     messageInput.value = '';
 })
 
+const joinRoomButton = document.getElementById('room-button');
+const roomInput = document.getElementById('room-input');
 joinRoomButton.addEventListener('click', () => {
     const room = roomInput.value;
     socket.emit('join-room', room, message => {
-        displayMessage(message);
+        displayRoom(message)
     });
 
 })
@@ -40,3 +40,68 @@ function displayMessage(message) {
     div.textContent = message;
     document.getElementById('message-container').append(div);
 }
+
+function displayRoom(room) {
+    const div = document.createElement('div');
+    div.textContent = room;
+    document.getElementById('room-container').append(div);
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.target.matches('input')) return
+    if (e.key === 'Enter') socket.connect()
+    if (e.key === 'd') socket.disconnect()
+})
+
+function displayAdminMessage(message, username ) {
+    const div = document.createElement('div');
+    div.textContent = username?`${username}: ${message}`: message;
+    document.getElementById('admin-container').append(div);
+}
+
+const adminLoginButton = document.getElementById('admin-login-button');
+const adminLoginInput = document.getElementById('admin-login-input');
+const adminPasswordInput = document.getElementById('admin-password-input');
+
+let adminSocket;
+adminLoginButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (adminSocket) adminSocket.disconnect()
+
+    adminSocket = io('http://localhost:3000/ad', {
+        auth: {
+            login: adminLoginInput.value,
+            password: adminPasswordInput.value
+        }
+    });
+
+    adminSocket.on('connect_error', (err) => {
+        const loginErrorPlace = document.getElementById('admin-login-error');
+        loginErrorPlace.textContent = err.message;
+    })
+
+    adminSocket.on('connect', () => {
+        displayAdminMessage(adminSocket.id + ' connected')
+        const loginErrorPlace = document.getElementById('admin-login-error');
+        loginErrorPlace.textContent = ''
+
+        const adminId = document.getElementById('myAdminID');
+        adminId.textContent = adminSocket.id;
+
+        adminSocket.on('receive-message', (username, message) => {
+            displayAdminMessage( message, username);
+        })
+        const sendAdminButton = document.getElementById('admin-send-button');
+        const adminMessageInput = document.getElementById('admin-message-input');
+
+        sendAdminButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            const message = adminMessageInput.value;
+            console.log(message)
+            if (message === "") return
+            adminSocket.emit('send-message', message);
+            adminMessageInput.value = '';
+        })
+    })
+})
+
--- server/server.js
+++ server/server.js
@@ -1,20 +1,50 @@
-const io = require('socket.io')(3000,{
+const {instrument} = require('@socket.io/admin-ui')
+const {Server} = require("socket.io");
+
+const io = new Server(3000, {
     cors: {
-        origin: ['http://localhost:8080']
-    }
+        origin: ['http://localhost:8080', 'https://admin.socket.io'],
+        credentials: true
+    },
 
 })
 
 io.on('connection', socket => {
     socket.on('send-message', (message, room) => {
-        if(room === ''){
+        if (room === '') {
             socket.broadcast.emit('receive-message', message)
-        }else{
+        } else {
             socket.to(room).emit('receive-message', message)
         }
     })
     socket.on('join-room', (room, cb) => {
         socket.join(room)
-        cb('joined room:  ' + room)
+        cb(room)
     })
 })
+
+const adminIo = io.of('/ad')
+
+
+adminIo.on('connection', socket => {
+    console.log('Connected to admin panel: ', socket.username)
+    socket.on('send-message', (message) => {
+        adminIo.emit('receive-message', socket.username, message)
+    })
+
+})
+
+const adminsData = {adlog1: 'adpas1', adlog2: 'adpas2', adlog3: 'adpas3'}
+
+adminIo.use((socket, next) => {
+    if (adminsData[socket.handshake.auth.login]
+        &&
+        adminsData[socket.handshake.auth.login] === socket.handshake.auth.password) {
+        socket.username = socket.handshake.auth.login
+        next()
+    } else
+        next(new Error('not authorized'))
+})
+
+instrument(io, {auth: false})
+
